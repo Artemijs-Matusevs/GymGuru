7a40678e96a888fb6a5937254a01eb902fd15154
Artemijs Matusev <matu6ev@gmail.com>
2024-09-03 10:30:51 +0100

    Setup template name changing functionality

--- public/scripts/workout-partials-script.js
+++ public/scripts/workout-partials-script.js
@@ -14,4 +14,33 @@ function workoutPartial(){
         mainContent.style.display = "block";
         newTemplateContent.style.display = "none";
     })
+
+    //Changing name for template
+    $(".partials-template-name-form").hide();
+
+
+    //On edit buton hide the h2 name and show the input field
+    $(".edit-name-button").on("click", function() {
+        //Set the name of the input box to the name of the template
+        $(".template-name-field").attr('value', $(".template-name").text());
+
+        $(".partials-template-name").hide();
+        $(".partials-template-name-form").show();
+    })
+
+    //Cancel edit name form
+    $(".cancel-name-button").on("click", function() {
+        $(".partials-template-name").show();
+        $(".partials-template-name-form").hide();
+    })
+
+    //Confirm edit name form
+    $(".confirm-name-button").on("click", function() {
+        //Set new name title
+        $(".template-name").text($(".template-name-field").val());
+
+        $(".partials-template-name").show();
+        $(".partials-template-name-form").hide();
+    })
+ 
 }
