c4ece046e62741b2a94d4322ea8b66d589e34bc7
Artemijs Matusev <matu6ev@gmail.com>
2024-09-03 14:15:05 +0100

    Implemented the exercise list on the front-end

--- src/models/workoutModel.js
+++ src/models/workoutModel.js
@@ -1,7 +1,7 @@
 import db from '../config/database.js';
 
 const getAllExercises = async ()=> {
-    const result = await db.query("SELECT * FROM exercises_dataset");
+    const result = await db.query("SELECT * FROM exercises_dataset ORDER BY exercise_name ASC");
     return result.rows;
 };
 
