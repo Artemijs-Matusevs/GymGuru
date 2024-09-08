68da9cdd91b9aa29a5ef259abe6f1a79e24fac0d
Artemijs Matusev <matu6ev@gmail.com>
2024-09-08 18:31:48 +0100

    Added new shema to workoutModel.js to insert new workout templates

--- src/models/workoutModel.js
+++ src/models/workoutModel.js
@@ -5,7 +5,18 @@ const getAllExercises = async ()=> {
     return result.rows;
 };
 
+//Create new record for the new workout template
+const addNewWorkoutTemplate = async(user_id, template_name) => {
+    try{
+        const result = await db.query("INSERT INTO workout_templates (user_id, template_name) VALUES ($1, $2)", [user_id, template_name]);
+        console.log("New workout template created");
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
 //exports
 export default{
     getAllExercises,
+    addNewWorkoutTemplate,
 }
